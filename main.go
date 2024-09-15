53b21aefd8b476c57a46ca8186f512999e3e931a
deepakkr35 <deepakkr35@gmail.com>
2024-09-15 23:35:48 +0530

    changes for running project in K8

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
+
+func getPodIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		// Check if the address is not a loopback and is an IP address
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("no IP address found")
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	// Render the home html page from static folder
@@ -25,12 +44,22 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/contact.html")
 }
 
+func podIPHandler(w http.ResponseWriter, r *http.Request) {
+    ip, err := getPodIP()
+    if err != nil {
+        http.Error(w, "Unable to get IP address", http.StatusInternalServerError)
+        return
+    }
+    fmt.Fprint(w, ip)
+}
+
 func main() {
 
-	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
+	http.HandleFunc("/pod-ip", podIPHandler) // New endpoint to serve IP address
 
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
